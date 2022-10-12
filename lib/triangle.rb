28a774978881f20f0efa94a8dc3aa8cd0e292e9d
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-12 17:42:19 -0400

    commit tree 65822ed2bf183d7fedfec1b89cf13da14c329d39

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c) 
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind 
+    validate_triangle
+
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero? 
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality? 
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
